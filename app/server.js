974c99990b2823d7e922a8ae37a677fa0e132019
Waheed Ahmed <waheedahmed55@gmail.com>
2022-01-08 06:46:58 -0500

    updated prod namespace

--- app/server.js
+++ app/server.js
@@ -26,7 +26,7 @@ db.sequelize.sync();
 
 // simple route
 app.get("/", (req, res) => {
-  res.json({ message: "Welcome to bezkoder application." });
+  res.json({ message: "Welcome to bezkoder application. prod env test" });
 });
 
 require("./app/routes/turorial.routes")(app);
